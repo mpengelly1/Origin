ff2ee4f9a2d623ff5118125ca5dce66d38ef60f9
Matthew Pengelly <mp810@cam.ac.uk>
2017-01-22 17:39:57 +0000

    Update geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -3,4 +3,17 @@ geographical data.
 
 """
 
+
+#Import modules from other files
 from .utils import sorted_by_key
+from .station import MonitoringStation
+
+
+def rivers_with_station(stations):
+  "Given a list of stations, returns all rivers (by name) with a monitoring station. Returns rivers as a set."
+  
+  
+def stations_by_river(stations):
+  "Assigns rivers as keys to stations specified within a python dict."
+  
+  {station.river : station.name for station in stations}  #Sets rive
