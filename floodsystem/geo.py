aef3be6fdcec3b52a6238fc30aae4f8247ebd65b
Kathe-Marie White <kmw54@cam.ac.uk>
2017-01-23 20:42:47 +0000

    creates list of tuples (station, distance) and returns sorted by the distance

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -5,3 +5,17 @@ geographical data.
 
 from .utils import sorted_by_key
 
+from haversine import haversine
+
+def stations_by_distance(stations, p):
+    """Sorts stations by distance. Stations is a list of stations, p is a tuple (latitude, longitude).
+    The function returns a list of tuples (station, distance)"""
+
+    station_distance = []
+
+    for s in stations:
+        #s.coord
+        distance = haversine(s.coord, p)
+        station_distance.append((s, distance))
+
+    return sorted_by_key(station_distance, 1)
