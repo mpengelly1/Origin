1fb1896ce22c25115d3153a0f72337d5176a58dc
Matthew Pengelly <mp810@cam.ac.uk>
2017-02-27 19:50:15 +0000

    updated analysis.py

---
+++ floodsystem/analysis.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib
+
+def polyfit(dates, levels, p):
+    """computes a least-squares fit of polynomial of degree p
+    to water level data. The function should return a tuple
+    of (1) the polynomial object and (2) any shift of the time (date) axis"""
+
+    # Find coefficients of best-fit polynomial
+    t = matplotlib.dates.date2num(dates)
+    p_coeff = np.polyfit(t - t[0], levels, p)
+
+    # Convert coefficient into a polynomial that can be evaluated
+
+    poly = np.poly1d(p_coeff)
+    matplotlib.pyplot.plot(t, levels, '.')
+
+    # Plot polynomial fit at 30 points along interval
+    t1 = np.linspace(t[0], t[-1], 30)
+    plt.plot(t1, poly(t1))
+
+    # Display plot
+    plt.show()
+
+polyfit(dates, levels, p)
