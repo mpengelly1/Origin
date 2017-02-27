de54f1c6496d3a6f9f55fb85e217e566884106e5
Matthew Pengelly <mp810@cam.ac.uk>
2017-02-27 23:15:45 +0000

    cleaned up task2e

--- Task2E.py
+++ Task2E.py
@@ -4,22 +4,35 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import update_water_levels
 import datetime
 
+
 def run():
     # Build list of stations
     stations = build_station_list()
 
     #generate list of levels, filtering out 'None' levels
     update_water_levels(stations)
-    level_list = [(station,station.latest_level) for station in stations if station.latest_level is not None]
+    level_list = [(station, station.latest_level) for station in stations if station.latest_level is not None]
 
     #sort by current level
-    level_list.sort(key = lambda x: x[1], reverse = True)
+    level_list.sort(key=lambda x: x[1], reverse=True)
 
     #plot top 5 stations
-    plot_data = list()
     dt = 10
     for station in level_list[:5]:
+        #get level history for each station
         dates, levels = fetch_measure_levels(station[0].measure_id, dt=datetime.timedelta(days=dt))
+
+        # Remove None values
+        for date in dates:
+            if date is None:
+                levels.remove(levels[dates.index(date)])
+                dates.remove(date)
+
+        for level in levels:
+            if level is None:
+                dates.remove(dates[levels.index(level)])
+                levels.remove(level)
+
         plot_water_levels(station[0],dates,levels)
 
 run()
