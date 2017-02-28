ab34bde63b8b1e673deee0fa81b4d5647d21b741
Kathe-Marie White <kmw54@cam.ac.uk>
2017-02-28 11:32:19 +0000

    edits

--- Task2F.py
+++ Task2F.py
@@ -1,8 +1,8 @@
-from floodsystem.plot import plot_water_levels
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list,update_water_levels
 from floodsystem.datafetcher import fetch_measure_levels
-from floodsystem.stationdata import update_water_levels
-import datetime
+from floodsystem.plot import plot_water_level_with_fit
+from datetime import datetime, timedelta
+
 
 
 def run():
@@ -11,18 +11,19 @@ def run():
 
     #generate list of levels, filtering out 'None' levels
     update_water_levels(stations)
-    level_list = [(station, station.latest_level) for station in stations if station.latest_level is not None]
+    level_list = [(station ,station.latest_level) for station in stations if station.latest_level is not None]
 
     #sort by current level
     level_list.sort(key=lambda x: x[1], reverse=True)
 
     #plot top 5 stations
-    dt = 10
-    for station in level_list[:5]:
-        #get level history for each station
-        dates, levels = fetch_measure_levels(station[0].measure_id, dt=datetime.timedelta(days=dt))
+    dt = 2   #get 2 days of data
 
-        # Remove None values
+
+    for station in level_list[:5]:
+        dates, levels = fetch_measure_levels(station[0].measure_id, dt=timedelta(days=dt))
+
+        #Remove None values
         for date in dates:
             if date is None:
                 levels.remove(levels[dates.index(date)])
@@ -33,6 +34,10 @@ def run():
                 dates.remove(dates[levels.index(level)])
                 levels.remove(level)
 
-        plot_water_levels(station[0],dates,levels)
+        #plot
+        plot_water_level_with_fit(station[0], dates, levels, 4)
+
+
+    plot_water_level_with_fit(stations[0],dates,levels,4)
 
 run()
---
+++ Task2G.py
@@ -0,0 +1,27 @@
+from floodsystem.flood import risk_level
+from floodsystem.stationdata import build_station_list
+from floodsystem.datafetcher import fetch_measure_levels
+from datetime import timedelta, datetime
+from plot import plot_water_level_with_fit
+
+
+def run():
+
+    # Build list of stations
+    stations = build_station_list()
+
+    #Categorise risk
+    risk_list = risk_level(stations[:100],0.8)
+
+    #Print top 10 most at risk
+    for station in risk_list[:10]:
+        print(station[0].name, station[2])
+
+    #Plot most at risk
+    dt = 3
+    dates, levels = fetch_measure_levels(risk_list[0][0].measure_id, dt=timedelta(days=dt))
+    plot_water_level_with_fit(risk_list[0][0], dates, levels, 4)
+
+
+
+run()
